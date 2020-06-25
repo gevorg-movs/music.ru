3c9302157f78c9a1ebd0e468b744860e8d3458f1
gevorg-movs <gevo14m@mail.ru>
2020-06-25 14:19:14 +0400

    Update

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
 session_start();
-$connection = new PDO('mysql:host=localhost;dbname=ca17463_music', 'ca17463_music', '123456789');
+$connection = new PDO('mysql:host=localhost;dbname=g67166_db', 'g67166_dbuser', 'RjDyWPZUXn&iSjCi');
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$connect = mysqli_connect('localhost', 'ca17463_music', '123456789', 'ca17463_music');  
+$connect = mysqli_connect('localhost', 'g67166_db', 'RjDyWPZUXn&iSjCi', 'g67166_dbuser');  
